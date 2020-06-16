0160b3ddad0eebd47ff9d4ace2ad2f7cb26008a4
neutralboy <leopoorna99@gmail.com>
2020-06-16 12:57:18 +0530

    seo and analytics

--- src/components/og.svelte
+++ src/components/og.svelte
@@ -2,10 +2,12 @@
     export let title = "";
     export let url = "";
     export let image = "https://res.cloudinary.com/poorna/image/upload/c_scale,q_auto:eco,w_100/v1590305977/undraw_medical_research_qg4d.png";
-    export let description = "Mednation is a community of medical students.";
+    export let description = "Mednation is a community of medical students. Become a contributing author and write a review for Mednation. ";
 </script>
 <svelte:head>
     <title>{title} | Mednation | A community of Medical Students</title>
+    <meta name="description" content={description}>
+    <meta name="keywords" content="Mednation, Medical Students, Community, College Reviews">
     <meta property="og:type" content="website" />
     <meta property="og:locale" content="en_IN" />
     <meta property="og:site_name" content="Mednation" />
@@ -13,5 +15,4 @@
     <meta property="og:url" content={"https://www.mednation.org/" + url} />
     <meta property="og:description" content={description} />
     <meta property="og:image" content={image} />
-    <meta property="og:image:type" content="image/png" />
 </svelte:head>
